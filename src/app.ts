8c885f80f597cfcaaee8845e84fb9d4b01e33617
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-07-02 14:08:19 +0545

    swaggerui api documentation

--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,18 @@ import taskRoutes from './routes/taskRoutes';
 import reminerRoutes from './routes/reminderRoutes';
 import cors from 'cors';
 
+// for swaggerUI API documentation
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "path";
 
+const swaggerDocument = YAML.load(path.join(__dirname, './docs/openapi.yaml'));
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use('/api/tasks',taskRoutes);
 app.use('/api/reminders',reminerRoutes);
+app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/health',(req,res)=>{
     res.status(200).json({message:'Server is running'});
